b0b39213c21b9487acf073832a1d7d00e076adde
Andres Gomez <agomez@cafetosoftware.com>
2017-01-13 15:56:01 -0500

    Another commit

--- database-tenant-script.sql
+++ database-tenant-script.sql
@@ -1,4 +1,4 @@
---
+--  
 -- This source code is the confidential, proprietary information of
 -- Cafeto Software S.A.S. here in, you may not disclose such Information,
 -- and may only use it in accordance with the terms of the license
